8d0b213babbf472e3223088839928487323b4309
MrDefi <karol@klycinski.com>
2026-04-06 20:07:32 +0200

    Usunieto ramke z ikon PWA

--- generate-icons.js
+++ generate-icons.js
@@ -18,17 +18,7 @@ function drawIcon(size, filename) {
   ctx.fillStyle = gradient;
   ctx.fillRect(0, 0, size, size);
 
-  // Border glow effect
-  ctx.strokeStyle = '#f472b6';
-  ctx.lineWidth = size * 0.03;
-  ctx.shadowColor = '#f472b6';
-  ctx.shadowBlur = size * 0.1;
-  ctx.beginPath();
-  ctx.roundRect(size * 0.08, size * 0.08, size * 0.84, size * 0.84, size * 0.12);
-  ctx.stroke();
-
   // Math symbols "+−" top and "×÷" bottom
-  ctx.shadowBlur = 0;
   ctx.fillStyle = '#f8fafc';
   ctx.font = `bold ${size * 0.35}px Arial`;
   ctx.textAlign = 'center';
